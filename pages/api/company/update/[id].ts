eebe37b6a6a1d981bc846ae2e3cecaaba6ce0b14
Vicky Salim <80747814+vickysalim@users.noreply.github.com>
2023-12-15 19:27:02 +0700

    fix update [id] company api

---
+++ pages/api/company/update/[id].ts
@@ -0,0 +1,56 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import api from '@/lib/api'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    api.method(req, res, `PUT`)
+
+    try {
+        const { name, street, district, subdistrict, city, province, postal_code, phone_number, leader, license } = req.body
+
+        if(!req.query.id) return api.res(res, 404, false, `ID is required`)
+
+        const data = await prisma.company.findUnique({
+            where: {
+                id: req.query.id as string
+            }
+        })
+
+        if(!data) return api.null(res, data, `Company`)
+
+        if(data.deleted_at) return api.null(res, data, `Company`)
+
+        if(postal_code && isNaN(postal_code))
+            return api.res(res, 404, false, `Postal code must be a number`)
+
+        await prisma.company.update({
+            where: {
+                id: req.query.id as string
+            },
+            data: {
+                name: name || data.name,
+                street: street || data.street,
+                district: district || data.district,
+                subdistrict: subdistrict || data.subdistrict,
+                city: city || data.city,
+                province: province || data.province,
+                postal_code: parseInt(postal_code) || data.postal_code,
+                phone_number: phone_number || data.phone_number,
+                leader: leader || data.leader,
+                license: license || data.license,
+                updated_at: new Date()
+            }
+        }).catch((err) => {
+            return api.res(res, 404, false, `Failed to update data`)
+        })
+
+        return api.res(res, 200, true, `Company updated`)
+    } catch (error) {
+        return api.error(res, error.message)
+    }
+}
