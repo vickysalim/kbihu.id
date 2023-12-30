4a431f1aa6f9075ad6354b7933308eb8e6978d05
Vicky Salim <80747814+vickysalim@users.noreply.github.com>
2023-12-31 00:54:44 +0700

    header fix height

--- views/components/Header/index.tsx
+++ views/components/Header/index.tsx
@@ -25,7 +25,7 @@ const Header = ({ role }: HeaderProps) => {
   }
 
   return (
-    <nav className="bg-white border-b border-grayscale-200 sticky top-0 z-50">
+    <nav className="bg-white border-b border-grayscale-200 sticky top-0 z-50 py-2">
         <div className="max-w-screen-xl mx-auto">
             <div className="flex items-center h-16 w-full">
                 <div className="flex items items-center mx-4 xl:mx-0 justify-between w-full">
