12f17f964bc4fb32737ce1d8ddb3d2e5be2fa46f
Vicky Salim <80747814+vickysalim@users.noreply.github.com>
2023-12-31 01:08:11 +0700

    format date add lib dateToPass

--- lib/date/format.ts
+++ lib/date/format.ts
@@ -2,4 +2,8 @@ const formatDate = (date: string) => {
     return new Date(date).toLocaleDateString('id-ID', { year: 'numeric', month: 'long', day: 'numeric' })
 }
 
-export default formatDate
+const dateToPass = (date: string) => {
+    return date.substring(0, 10).replace(/-/g, '')
+}
+
+export { formatDate, dateToPass }
