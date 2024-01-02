e646fb0ae562c8d82930a40d87f4498fab0ed075
Vicky Salim <80747814+vickysalim@users.noreply.github.com>
2024-01-03 02:35:32 +0700

    format date lib for input date

--- lib/date/format.ts
+++ lib/date/format.ts
@@ -2,8 +2,16 @@ const formatDate = (date: string) => {
     return new Date(date).toLocaleDateString('id-ID', { year: 'numeric', month: 'long', day: 'numeric' })
 }
 
+const formatDateInput = (timestamp: string) => {
+    let date = new Date(timestamp);
+    let year = date.getFullYear();
+    let month = (1 + date.getMonth()).toString().padStart(2, '0');
+    let day = date.getDate().toString().padStart(2, '0');
+    return year + '-' + month + '-' + day
+}
+
 const dateToPass = (date: string) => {
     return date.substring(0, 10).replace(/-/g, '')
 }
 
-export { formatDate, dateToPass }
+export { formatDate, formatDateInput, dateToPass }
