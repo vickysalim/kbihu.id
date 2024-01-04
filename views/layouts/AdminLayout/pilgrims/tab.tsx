eab732996bb5b037cb2be905edd63a635af46e24
Vicky Salim <80747814+vickysalim@users.noreply.github.com>
2024-01-04 09:45:56 +0700

    pilgrim dynamic tab styling

--- views/layouts/AdminLayout/pilgrims/tab.tsx
+++ views/layouts/AdminLayout/pilgrims/tab.tsx
@@ -1,14 +1,33 @@
 interface AdminPilgrimsTabLayoutProps {
+    activeTab: 'data' | 'payment' | 'document' | 'facility'
     id: string | string[] | undefined
 }
 
-const AdminPilgrimsTabLayout = ({ id }: AdminPilgrimsTabLayoutProps) => {
+const AdminPilgrimsTabLayout = ({ id, activeTab }: AdminPilgrimsTabLayoutProps) => {
+    const activeTabStyle = 'border-b-blue-500 text-blue-500'
+    const inactiveTabStyle = 'text-slate-800'
     return (
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4'>
-            <button className='border-b-4 border-b-blue-500 text-blue-500 rounded-lg p-2 mb-4 text-center font-semibold' onClick={() => window.location.href = `/dashboard/pilgrims/${id}`}>Data Jemaah Haji</button>
-            <button className='border-b-4 text-slate-800 rounded-lg p-2 mb-4 text-center font-semibold'>Data Pembayaran</button>
-            <button className='border-b-4 rounded-lg p-2 mb-4 text-center font-semibold'>Data Dokumen</button>
-            <button className='border-b-4 rounded-lg p-2 mb-4 text-center font-semibold'>Data Fasilitas</button>
+            <button
+                className={`border-b-4 ${activeTab == 'data' ? activeTabStyle : inactiveTabStyle} rounded-lg p-2 mb-4 text-center font-semibold`}
+                onClick={() => window.location.href = `/dashboard/pilgrims/${id}`}>
+                    Data Jemaah Haji
+            </button>
+            <button
+                className={`border-b-4 ${activeTab == 'payment' ? activeTabStyle : inactiveTabStyle} rounded-lg p-2 mb-4 text-center font-semibold`}
+                onClick={() => window.location.href = `/dashboard/pilgrims/payment/${id}`}>
+                    Data Pembayaran
+            </button>
+            <button
+                className={`border-b-4 ${activeTab == 'document' ? activeTabStyle : inactiveTabStyle} rounded-lg p-2 mb-4 text-center font-semibold`}
+                >
+                    Data Dokumen
+            </button>
+            <button
+                className={`border-b-4 ${activeTab == 'facility' ? activeTabStyle : inactiveTabStyle} rounded-lg p-2 mb-4 text-center font-semibold`}
+                >
+                    Data Fasilitas
+            </button>
         </div>
     )
 }
