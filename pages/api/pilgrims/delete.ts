2140c92e8d60fd825fb8a170809ed3fc788919a9
Vicky Salim <80747814+vickysalim@users.noreply.github.com>
2024-01-04 09:23:31 +0700

    api delete pilgrims

---
+++ pages/api/pilgrims/delete.ts
@@ -0,0 +1,54 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { z } from 'zod'
+import api from '@/lib/api'
+import prisma from '@/lib/prisma'
+
+const validation = z.object({
+    id: z.string().min(1, { message: `ID is required` })
+})
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    api.method(req, res, `DELETE`)
+
+    try {
+        const reqBody = validation.parse(req.body)
+
+        await prisma.user_account.update({
+            where: {
+                id: reqBody.id
+            },
+            data: {
+                deleted_at: new Date()
+            }
+        }).catch((err) => {
+            return api.res(res, 404, false, `User not found`)
+        })
+
+        await prisma.user_profile.updateMany({
+            where: {
+                user_account_id: reqBody.id
+            },
+            data: {
+                deleted_at: new Date()
+            }
+        }).catch((err) => {
+            return api.res(res, 404, false, `User not found`)
+        })
+
+        return api.res(res, 200, true, `User deleted`)
+    } catch (error) {
+        if(error instanceof z.ZodError) {
+            const validation = error.errors.map((error) => ({
+                field: error.path[0],
+                message: error.message
+            }))
+
+            return api.invalid(res, validation)
+        } else {
+            return api.error(res, error.message)
+        }
+    }
+}
