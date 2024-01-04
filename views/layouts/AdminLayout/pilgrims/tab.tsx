9965be48ba171aa6e6eeeb8e632842ad415964c4
Vicky Salim <80747814+vickysalim@users.noreply.github.com>
2024-01-04 09:34:41 +0700

    pilgrim tab set id props

--- views/layouts/AdminLayout/pilgrims/tab.tsx
+++ views/layouts/AdminLayout/pilgrims/tab.tsx
@@ -1,7 +1,11 @@
-const AdminPilgrimsTabLayout: React.FC = () => {
+interface AdminPilgrimsTabLayoutProps {
+    id: string | string[] | undefined
+}
+
+const AdminPilgrimsTabLayout = ({ id }: AdminPilgrimsTabLayoutProps) => {
     return (
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4'>
-            <button className='border-b-4 border-b-blue-500 text-blue-500 rounded-lg p-2 mb-4 text-center font-semibold'>Data Jemaah Haji</button>
+            <button className='border-b-4 border-b-blue-500 text-blue-500 rounded-lg p-2 mb-4 text-center font-semibold' onClick={() => window.location.href = `/dashboard/pilgrims/${id}`}>Data Jemaah Haji</button>
             <button className='border-b-4 text-slate-800 rounded-lg p-2 mb-4 text-center font-semibold'>Data Pembayaran</button>
             <button className='border-b-4 rounded-lg p-2 mb-4 text-center font-semibold'>Data Dokumen</button>
             <button className='border-b-4 rounded-lg p-2 mb-4 text-center font-semibold'>Data Fasilitas</button>
