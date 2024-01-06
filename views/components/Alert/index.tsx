c6f95afd68b18932fea0e280f7d92ca1c58ac86e
Vicky Salim <80747814+vickysalim@users.noreply.github.com>
2024-01-07 06:02:37 +0700

    remove close button alert

--- views/components/Alert/index.tsx
+++ views/components/Alert/index.tsx
@@ -22,11 +22,6 @@ const Alert = ({ children, type }: AlertProps) => {
                 <div>
                     {children}
                 </div>
-                <button onClick={() => setIsClosed(true)} className='float-right'>
-                    <svg className='w-6 h-6' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
-                        <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M6 18L18 6M6 6l12 12' />
-                    </svg>
-                </button>
             </div>
         </Transition>
     )
