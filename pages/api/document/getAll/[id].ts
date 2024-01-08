263df3a1b8325ddfe8b2e7b8f183c740efe216c0
Vicky Salim <80747814+vickysalim@users.noreply.github.com>
2024-01-08 17:27:08 +0700

    api to get all document

---
+++ pages/api/document/getAll/[id].ts
@@ -0,0 +1,48 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { z } from 'zod'
+import api from '@/lib/api'
+import prisma from '@/lib/prisma'
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    api.method(req, res, 'GET')
+
+    try {
+        if(!req.query.id) return api.res(res, 404, false, `ID is required`)
+
+        const document = await prisma.company_document.findMany({
+            select: {
+                id: true,
+                name: true,
+                company_document_year: {
+                    select: {
+                        id: true,
+                        year: true,
+                    }
+                }
+            },
+            where: {
+                deleted_at: null,
+                company_id: req.query.id as string
+            },
+        })
+
+        if(!document) api.null(res, document, 'Document')
+
+        return api.res(res, 200, true, 'Get document data success', document)
+
+    } catch (error: any) {
+        if(error instanceof z.ZodError) {
+            const validation = error.errors.map((error) => ({
+                field: error.path[0],
+                message: error.message
+            }))
+
+            return api.invalid(res, validation)
+        } else {
+            return api.error(res, error.message)
+        }
+    }
+}
